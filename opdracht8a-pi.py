7be799b02aae5ae62bbe35bc5a7524df63bd702e
Maxim Jansen <45556790+MaximRSD@users.noreply.github.com>
2025-07-07 15:38:18 +0200

    Update opdracht8a-pi.py

--- opdracht8a-pi.py
+++ opdracht8a-pi.py
@@ -1,20 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-SIGNAL_PIN = 17  # GPIO17, stuurt HIGH/LOW signaal naar Arduino
+LED1 = 17  # Naar Arduino inputPin1
+LED2 = 27  # Naar Arduino inputPin2
 
-GPIO.setup(SIGNAL_PIN, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
 
 try:
     while True:
-        print("Signaal NAAR ARDUINO: AAN")
-        GPIO.output(SIGNAL_PIN, GPIO.HIGH)
-        time.sleep(1)
-        print("Signaal NAAR ARDUINO: UIT")
-        GPIO.output(SIGNAL_PIN, GPIO.LOW)
+        print("LED1 aan, LED2 uit")
+        GPIO.output(LED1, GPIO.HIGH)
+        GPIO.output(LED2, GPIO.LOW)
         time.sleep(1)
 
-except KeyboardInterrupt:a
+        print("LED1 uit, LED2 aan")
+        GPIO.output(LED1, GPIO.LOW)
+        GPIO.output(LED2, GPIO.HIGH)
+        time.sleep(1)
+
+except KeyboardInterrupt:
     GPIO.cleanup()
-    print("GPIO opgeruimd")
+    print("GPIO opgeruimd, programma gestopt")
